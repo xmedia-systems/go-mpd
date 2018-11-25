69c935308e29fab62dc1edba6f92008a8141c00e
Martin Fillafer <martin.fillafer@bitmovin.com>
2018-11-25 19:58:46 +0100

    Make unit tests more generic to also test the new implementation

--- xsd/duration_test.go
+++ xsd/duration_test.go
@@ -31,42 +31,58 @@ func TestDuration_String(t *testing.T) {
 	assert.Equal(t, "-P1Y1DT1H1M1S", Duration(-(366*24*hour + hour + 61*sec)).String())
 }
 
-func checkDurationFromString(t *testing.T, str string, val int) {
-	dur, err := DurationFromString(str)
+type DurationFromStringType func(string) (*Duration, error)
+
+func checkDurationFromString(t *testing.T, fromString DurationFromStringType, str string, val int) {
+	dur, err := fromString(str)
 	assert.Nil(t, err)
 	assert.Equal(t, Duration(val), *dur)
 }
 
+func checkDurationWithMethod(t *testing.T, fromString DurationFromStringType) {
+	checkDurationFromString(t, fromString, "PT0S", 0)
+	checkDurationFromString(t, fromString, "PT1S", sec)
+	checkDurationFromString(t, fromString, "PT0.11S", 110*msec)
+
+	checkDurationFromString(t, fromString, "PT1M", 60*sec)
+	checkDurationFromString(t, fromString, "PT1M1S", 61*sec)
+	checkDurationFromString(t, fromString, "PT1M1.1S", 61*sec+100*msec)
+	checkDurationFromString(t, fromString, "PT1H", hour)
+	checkDurationFromString(t, fromString, "PT1H1M", hour+60*sec)
+	checkDurationFromString(t, fromString, "PT1H1M1S", hour+61*sec)
+	checkDurationFromString(t, fromString, "P1D", 24*hour)
+	checkDurationFromString(t, fromString, "P1DT1H1M1S", 25*hour+61*sec)
+	checkDurationFromString(t, fromString, "P1Y", 365*24*hour)
+	checkDurationFromString(t, fromString, "P1Y1DT1H1M1S", 366*24*hour+hour+61*sec)
+
+	checkDurationFromString(t, fromString, "-PT1S", -sec)
+	checkDurationFromString(t, fromString, "-P1Y1DT1H1M1S", -(366*24*hour + hour + 61*sec))
+
+	_, err := fromString("PT")
+	assert.Equal(t, invalidFormatError, err)
+
+	_, err = fromString("P1.")
+	assert.Equal(t, invalidFormatError, err)
+
+	_, err = fromString("PT1.")
+	assert.Equal(t, invalidFormatError, err)
+
+	_, err = fromString("PT1.S")
+	assert.Equal(t, invalidFormatError, err)
+
+	_, err = fromString("xPT1S")
+	assert.Equal(t, invalidFormatError, err)
+
+	_, err = fromString("PT1Sx")
+	assert.Equal(t, invalidFormatError, err)
+}
+
 func TestDurationFromString(t *testing.T) {
-	checkDurationFromString(t, "PT0S", 0)
-	checkDurationFromString(t, "PT1S", sec)
-	checkDurationFromString(t, "PT0.11S", 110*msec)
+	checkDurationWithMethod(t, DurationFromString)
+}
 
-	checkDurationFromString(t, "PT1M", 60*sec)
-	checkDurationFromString(t, "PT1M1S", 61*sec)
-	checkDurationFromString(t, "PT1M1.1S", 61*sec+100*msec)
-	checkDurationFromString(t, "PT1H", hour)
-	checkDurationFromString(t, "PT1H1M", hour+60*sec)
-	checkDurationFromString(t, "PT1H1M1S", hour+61*sec)
-	checkDurationFromString(t, "P1D", 24*hour)
-	checkDurationFromString(t, "P1DT1H1M1S", 25*hour+61*sec)
-	checkDurationFromString(t, "P1Y", 365*24*hour)
-	checkDurationFromString(t, "P1Y1DT1H1M1S", 366*24*hour+hour+61*sec)
-
-	checkDurationFromString(t, "-PT1S", -sec)
-	checkDurationFromString(t, "-P1Y1DT1H1M1S", -(366*24*hour + hour + 61*sec))
-
-	_, err := DurationFromString("PT")
-	assert.Equal(t, invalidFormatError, err)
-
-	_, err = DurationFromString("P1.")
-	assert.Equal(t, invalidFormatError, err)
-
-	_, err = DurationFromString("PT1.")
-	assert.Equal(t, invalidFormatError, err)
-
-	_, err = DurationFromString("PT1.S")
-	assert.Equal(t, invalidFormatError, err)
+func TestDurationFromString2(t *testing.T) {
+	checkDurationWithMethod(t, DurationFromString2)
 }
 
 type DurationAttr struct {
@@ -101,6 +117,12 @@ func BenchmarkDuration_String(b *testing.B) {
 
 func BenchmarkDurationFromString(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_, _ = DurationFromString("P1Y1DT1H1M1S")
+		_, _ = DurationFromString("P1Y2DT3H4M5.67S")
+	}
+}
+
+func BenchmarkDurationFromString2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _ = DurationFromString2("P1Y2DT3H4M5.67S")
 	}
 }
